a622394f4095c0a374d4cd71ce5efb85c540910e
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2018-03-07 15:07:57 -0700

    add face recognition

--- src/App.js
+++ src/App.js
@@ -1,41 +1,81 @@
 import React, {Component} from 'react';
 import Particles from 'react-particles-js';
+import Clarifai  from 'clarifai';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
 import './App.css';
 
+const app = new Clarifai.App({
+    apiKey: 'cda9cc3c98924dd4b66091da62a2e39f'
+});
+
 const particlesOptions = {
     particles: {
-   number:{
-       value: 100,
-       density:{
-           enable: true,
-           value_area: 800
-       }
-   }
+        number: {
+            value: 300,
+            density: {
+                enable: true,
+                value_area: 800
+            }
+        }
     }
 };
 
 
 class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            input: '',
+            imageUrl: '',
+            box: {}
+        }
+    }
+
+  calculateFaceLocation = (data) =>{
+      const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+      const image = document.getElementById('inputImage');
+      const width = Number(image.width);
+      const height = Number(image.height);
+      return {
+          leftCol: clarifaiFace.left_col * width,
+          topRow: clarifaiFace.top_row * height,
+          rightCol:  width - (clarifaiFace.right_col * width),
+          bottomRow: height - (clarifaiFace.bottom_row * height)
+      }
+  };
+
+  displayFaceBox = (box) => {
+      this.setState({box: box});
+  }
+    onInputChange = (event) => {
+        this.setState({input: event.target.value});
+    };
+
+    onButtonSubmit = () => {
+        this.setState({imageUrl: this.state.input});
+        app.models.predict(
+            Clarifai.FACE_DETECT_MODEL,
+            this.state.input)
+            .then (response => this.displayFaceBox (this.calculateFaceLocation(response)))
+            .catch (err => console.log(err));
+    };
+
     render() {
         return (
             <div className="App">
                 <Particles className="particles"
-                    params={particlesOptions}
+                           params={particlesOptions}
                 />
                 <Navigation />
                 <Logo />
                 <Rank />
-                <ImageLinkForm />
+                <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
+                <FaceRecognition box={this.state.box} imageUrl={this.state.imageUrl}/>
 
-                {/*{*/}
-                {/*<Logo />*/}
-                {/**/}
-                {/*<FaceRecognition />*/}
-                {/*}*/}
 
             </div>
         );
