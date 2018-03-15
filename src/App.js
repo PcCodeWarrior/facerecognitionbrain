1bf2d94fc5135f5966e16680b2caaa8bf1da6a29
PcCodeWarrior <PC_Code_Warrior@yahoo.com>
2018-03-15 16:02:59 -0600

    refactor front end

--- src/App.js
+++ src/App.js
@@ -27,25 +27,29 @@ const particlesOptions = {
 };
 
 
+const initialState ={
+    input: '',
+    imageUrl: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user:{
+        id: '',
+        name: '',
+        email: '',
+        password: '',
+        entries: 0,
+        joined: ''
+    }
+}
+
+
 class App extends Component {
     constructor() {
         super();
-        this.state = {
-            input: '',
-            imageUrl: '',
-            box: {},
-            route: 'signin',
-            isSignedIn: false,
-            user:{
-                id: '',
-                name: '',
-                email: '',
-                password: '',
-                entries: 0,
-                joined: ''
-            }
-        }
+        this.state = initialState;
     }
+
 
     loadUser = (data) => {
         this.setState({user: {
@@ -95,6 +99,7 @@ class App extends Component {
                         .then(count => {
                             this.setState(Object.assign(this.state.user,{entries:count}))
                         })
+                        .catch(console.log)
                 }
                 this.displayFaceBox(this.calculateFaceLocation(response))
             })
@@ -103,7 +108,7 @@ class App extends Component {
 
     onRouteChange = (route) => {
         if (route === 'signout'){
-            this.setState({isSignedIn: false})
+            this.setState(initialState)
         } else if (route === 'home'){
             this.setState({isSignedIn: true})
         }
